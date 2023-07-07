40fccbda9bf79b8c964c1d96b7ed7f788263d193
cedric karungu <ckarungu921@gmail.com>
2023-07-07 12:25:43 +0200

    feat: add models

--- src/boat/boat.service.ts
+++ src/boat/boat.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from './../prisma/prisma.service';
 import { Agence, Boat } from '@prisma/client';
 
@@ -7,36 +11,62 @@ export class BoatService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async findAll(): Promise<Boat[]> {
-    return await this.prismaService.boat.findMany();
+    try {
+      return await this.prismaService.boat.findMany();
+    } catch (err: unknown) {
+      throw new InternalServerErrorException();
+    }
   }
 
   async findOne(id: string): Promise<Boat> {
-    return await this.prismaService.boat.findUnique({
-      where: { id },
-      include: { agence: true },
-    });
+    try {
+      return await this.prismaService.boat.findUnique({
+        where: { id },
+        include: { agence: true },
+      });
+    } catch (err: unknown) {
+      throw new InternalServerErrorException();
+    }
   }
 
   async findByAgence(idAgence: string): Promise<Agence & { boats: Boat[] }> {
-    return await this.prismaService.agence.findUnique({
-      where: { id: idAgence },
-      include: { boats: true },
-    });
+    try {
+      return await this.prismaService.agence.findUnique({
+        where: { id: idAgence },
+        include: { boats: true },
+      });
+    } catch (err: unknown) {
+      throw new InternalServerErrorException();
+    }
   }
 
   async createBoat(idAgence: string, data: any): Promise<Boat> {
-    return await this.prismaService.boat.create({
-      data: {
-        agenceId: idAgence,
-        ...data,
-      },
-    });
+    try {
+      const agence: Agence = await this.prismaService.agence.findUnique({
+        where: { id: idAgence },
+      });
+
+      if (!agence) throw new BadRequestException();
+
+      return await this.prismaService.boat.create({
+        data: {
+          agenceId: idAgence,
+          ...data,
+        },
+      });
+    } catch (err: unknown) {
+      throw new InternalServerErrorException();
+    }
   }
 
   async updateBoat(id: string, data: any) {
-    return await this.prismaService.boat.update({
-      data,
-      where: { id },
-    });
+    try {
+      return await this.prismaService.boat.update({
+        data,
+        where: { id },
+      });
+    } catch (err: unknown) {
+      throw new InternalServerErrorException();
+    }
   }
 }
