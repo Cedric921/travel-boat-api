2988cb78142eaff2ea7bc5e76bdd79b75a2ced47
cedric karungu <ckarungu921@gmail.com>
2023-07-13 15:01:23 +0200

    feat: add users routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getAll() {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string) {
+    return this.userService.getOne(id);
+  }
+
+  @Post()
+  createUser(@Body() dto: any) {
+    return this.userService.create(dto);
+  }
+
+  @Put(':id')
+  updateById(@Param('id') id: string, @Body() dto: any) {
+    return this.userService.updateById(id, dto);
+  }
+}
