04c413a1eea55dd66d710f7ea45572f9a5ceef01
cedric karungu <ckarungu921@gmail.com>
2023-07-14 12:14:07 +0200

    feat: add get user Decorator

---
+++ src/auth/decorator/getUser.decorator..ts
@@ -0,0 +1,9 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+
+    return req.user;
+  },
+);
