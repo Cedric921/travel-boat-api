48fdafda67e9cd1fbf82f1904757ca7035e9512a
cedric karungu <ckarungu921@gmail.com>
2023-07-14 19:41:48 +0200

    feat: add tickets routes

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { TicketService } from './ticket.service';
 
-@Controller('ticket')
-export class TicketController {}
+@Controller('tickets')
+export class TicketController {
+  constructor(private readonly ticketService: TicketService) {}
+
+  @Get()
+  getAll() {
+    return this.ticketService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.ticketService.getOne(id);
+  }
+
+  @Post()
+  createTicket(@Body() ticketDto: any) {
+    return this.ticketService.createTicket(ticketDto);
+  }
+
+  @Put(':id')
+  updateById(@Param('id') id: string, @Body() ticketDto: any) {
+    return this.ticketService.update(id, ticketDto);
+  }
+}
