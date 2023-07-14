8c4ef4ccead9d4f3bf40d600b03a7fc9606d1af5
cedric karungu <ckarungu921@gmail.com>
2023-07-14 14:08:28 +0200

    feat: check user from strategy

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class SignupDTO {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  names: string;
+}
+
+export class LoginDTO {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
