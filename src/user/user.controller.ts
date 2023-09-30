626090b37b6be5d8ed6f3d671623474e0c70a868
cedric karungu <ckarungu921@gmail.com>
2023-09-30 21:46:03 +0200

    feat: get admins route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -17,31 +17,37 @@ import { User } from '@prisma/client';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'SUPER_ADMIN']))
+  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'ADMIN']))
   @Get()
   getAll() {
     return this.userService.getAll();
   }
 
-  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'SUPER_ADMIN']))
+  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'ADMIN']))
   @Get('agence')
   getCompanyUsers(@GetUser() user: User) {
     return this.userService.getAgenceUsers(user.agenceId);
   }
 
-  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'SUPER_ADMIN']))
+  @UseGuards(AuthGuard('jwt'), new RoleGuard(['ADMIN']))
+  @Get('admins')
+  getAdmins() {
+    return this.userService.getAdmins();
+  }
+
+  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'ADMIN']))
   @Get(':id')
   getById(@Param('id') id: string) {
     return this.userService.getOne(id);
   }
 
-  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'SUPER_ADMIN']))
+  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'ADMIN']))
   @Post()
   createUser(@Body() dto: any, @GetUser() user: User) {
     return this.userService.create({ ...dto, agenceId: user.agenceId });
   }
 
-  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'SUPER_ADMIN']))
+  @UseGuards(AuthGuard('jwt'), new RoleGuard(['USER', 'ADMIN']))
   @Put(':id')
   updateById(@Param('id') id: string, @Body() dto: any) {
     return this.userService.updateById(id, dto);
